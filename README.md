c2813aad215942b8626534e813dad5560f57c364
FraSharp <s23265@iisve.it>
2022-03-01 22:23:08 +0100

    fastboot_python: import
    Signed-off-by: FraSharp <s23265@iisve.it>

---
+++ README.md
@@ -0,0 +1,7 @@
+A simple script in python to flash a custom rom for android with fastboot
+
+## Usage
+python3 flashall.py
+
+## Note
+You have to put flashall.py in the same directory where the images are
---
+++ flashall.py
@@ -0,0 +1,36 @@
+import os.path
+import os
+import time
+import getopt
+import sys
+
+# These are the partitions on most A/B devices with fastbootd support, this is script is made for
+# OnePlus 8T but can be adapted for any device
+partitions = ("boot", "dtbo", "recovery", "vbmeta", "vbmeta_system", "system", "product", "system_ext", "odm", "vendor")
+
+# Fancy colors
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+print("\n" + color.BOLD + color.UNDERLINE + color.CYAN + "2021-2022\nby FraSharp @ GitHub.com" + color.END)
+
+print(color.RED + "\npreparing to flash rom, 5 seconds to abort..." + color.END)
+time.sleep(5)
+
+print(color.RED + "starting to flash rom...\n" + color.END)
+time.sleep(1)
+
+for partition in partitions:
+	if not os.path.exists("./" + partition + ".img"):
+		print(color.BOLD + color.PURPLE + "no", partition, "to flash" + color.END)
+	elif partition == "vbmeta" or partition == "vbmeta_system":
+		os.system("fastboot flash --disable-verity " + partition + " " + partition + ".img")
+	else:
+		os.system("fastboot flash " + partition + " " + partition + ".img")
+
+print("\n" + color.BOLD + color.RED + "format data if coming from another rom, else just reboot" + color.END)
